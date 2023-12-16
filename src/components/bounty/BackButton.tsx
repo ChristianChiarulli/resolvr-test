d83b56ca297a3cd1bc86774928c6c8be6f27d701
Christian Chiarulli <chris.machine@pm.me>
2023-12-16 06:45:14 -0500

    update back button

--- src/components/bounty/BackButton.tsx
+++ src/components/bounty/BackButton.tsx
@@ -7,7 +7,7 @@ export default function BackButton() {
   return (
     <Link href="/">
       {/* TODO: route back if routing back to / if not just route to / */}
-      <Button className="flex bg-secondary/70 hover:bg-secondary/60">
+      <Button variant="outline" className="flex dark:bg-secondary/70 dark:hover:bg-secondary/60">
         <ArrowLeftIcon className="h-4 w-4 mr-1" />
         Back to all Bounties
       </Button>
