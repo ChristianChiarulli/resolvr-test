287fff1a8d75a73d5e07d74735e5a35783631d96
Christian Chiarulli <chris.machine@pm.me>
2023-12-18 03:50:30 -0500

    filter and tag cleanup

--- src/components/bounty-feed/BountyTags.tsx
+++ src/components/bounty-feed/BountyTags.tsx
@@ -5,9 +5,7 @@ import React, { useEffect, useState } from "react";
 import { TAGS } from "~/lib/constants";
 import {
   useParams,
-  usePathname,
   useRouter,
-  useSearchParams,
 } from "next/navigation";
 
 import {
@@ -20,25 +18,19 @@ import {
 
 export default function BountyTags() {
   const router = useRouter();
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
   const params = useParams();
   const [selectedTag, setSelectedTag] = useState<string>("");
 
   useEffect(() => {
-    // const tagFromURL = params.get('tag');
     const tagFromURL = params.tag;
     if (tagFromURL && typeof tagFromURL === "string") {
       setSelectedTag(tagFromURL);
     } else {
-      setSelectedTag(""); // Reset to default value if 'tag' is not in URL
+      setSelectedTag("");
     }
-  }, [searchParams]);
+  }, [params.tag]);
 
   const handleValueChange = (newTag: string) => {
-    // const updatedParams = new URLSearchParams(searchParams.toString());
-    // updatedParams.set('tag', newTag);
-    // router.push(`${pathname}?${updatedParams.toString()}`);
     if (newTag === "all") {
       router.push("/");
     } else router.push(`/tag/${newTag}`);
