f123c42138916c6b6ec8b05cb5c5d37f65b18b94
Christian Chiarulli <chris.machine@pm.me>
2023-12-19 03:53:06 -0500

    discussion tab

--- src/components/bounty/BountyTabs.tsx
+++ src/components/bounty/BountyTabs.tsx
@@ -1,5 +1,7 @@
-import { capitalizeFirstLetter, cn, fromNow } from "~/lib/utils";
-import { BookOpen, LockIcon, Users } from "lucide-react";
+import useAuth from "~/hooks/useAuth";
+import { cn, fromNow } from "~/lib/utils";
+import nq from "~/nostr-query";
+import { BookOpen, LockIcon, MessagesSquare, Users } from "lucide-react";
 import Link from "next/link";
 import { type Event } from "nostr-tools";
 
@@ -7,18 +9,112 @@ import ApplicantFeed from "../applications/ApplicantFeed";
 import ApplicationCount from "../applications/ApplicationCount";
 import BountyDetails from "./BountyDetails";
 
-type Props = {
+type BountyTabsProps = {
   bounty: Event | undefined;
   selectedTab: string;
 };
 
-const tabs = [
-  { name: "details", icon: BookOpen },
-  { name: "applications", icon: Users },
-  { name: "discussion", icon: LockIcon },
-];
+type TabProps = {
+  selectedTab: string;
+  bounty: Event | undefined;
+};
 
-export default function BountyTabs({ bounty, selectedTab }: Props) {
+function DetailTab({ selectedTab }: TabProps) {
+  return (
+    <Link
+      replace={true}
+      key={"details"}
+      href={"?tab=details"}
+      className={cn(
+        selectedTab === "details"
+          ? "border-indigo-600 text-indigo-600 dark:border-indigo-500 dark:text-indigo-500"
+          : "border-transparent text-muted-foreground hover:border-primary-foreground hover:text-primary-foreground",
+        "group inline-flex items-center border-b px-1 py-4 text-sm font-medium",
+      )}
+    >
+      <BookOpen
+        className={cn(
+          selectedTab === "details"
+            ? "text-indigo-600 dark:text-indigo-500"
+            : "text-muted-foreground group-hover:text-primary-foreground",
+          "-ml-0.5 mr-2 h-5 w-5",
+        )}
+      />
+      <span>Details</span>
+    </Link>
+  );
+}
+
+function ApplicationTab({ selectedTab, bounty }: TabProps) {
+  return (
+    <Link
+      replace={true}
+      href={"?tab=applications"}
+      className={cn(
+        selectedTab === "applications"
+          ? "border-indigo-600 text-indigo-600 dark:border-indigo-500 dark:text-indigo-500"
+          : "border-transparent text-muted-foreground hover:border-primary-foreground hover:text-primary-foreground",
+        "group inline-flex items-center border-b px-1 py-4 text-sm font-medium",
+      )}
+    >
+      <Users
+        className={cn(
+          selectedTab === "applications"
+            ? "text-indigo-600 dark:text-indigo-500"
+            : "text-muted-foreground group-hover:text-primary-foreground",
+          "-ml-0.5 mr-2 h-5 w-5",
+        )}
+      />
+      <span className="flex gap-x-1">
+        {bounty && <ApplicationCount bounty={bounty} />}
+        Applications
+      </span>
+    </Link>
+  );
+}
+
+// to show for logged in user bounty must have p tag, and you must be signed in
+// to show for applicant bounty must have p tag, and it must match your logged in pubkey
+function DiscussionTab({ selectedTab, bounty }: TabProps) {
+  const { pubkey } = useAuth();
+
+  if (
+    // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
+    (pubkey === bounty?.pubkey && nq.tag("p", bounty)) ||
+    pubkey === nq.tag("p", bounty)
+  ) {
+    return (
+      <Link
+        replace={true}
+        href={"?tab=discussion"}
+        className={cn(
+          selectedTab === "discussion"
+            ? "border-indigo-600 text-indigo-600 dark:border-indigo-500 dark:text-indigo-500"
+            : "border-transparent text-muted-foreground hover:border-primary-foreground hover:text-primary-foreground",
+          "group inline-flex items-center border-b px-1 py-4 text-sm font-medium",
+        )}
+      >
+        <MessagesSquare
+          className={cn(
+            selectedTab === "discussion"
+              ? "text-indigo-600 dark:text-indigo-500"
+              : "text-muted-foreground group-hover:text-primary-foreground",
+            "-ml-0.5 mr-2 h-5 w-5",
+          )}
+        />
+        <span>Discussion</span>
+      </Link>
+    );
+  }
+  return (
+    <span className="group inline-flex cursor-not-allowed items-center border-b border-transparent px-1 py-4 text-sm font-medium text-muted-foreground">
+      <LockIcon className="-ml-0.5 mr-2 h-5 w-5 text-muted-foreground" />
+      <span>Discussion</span>
+    </span>
+  );
+}
+
+export default function BountyTabs({ bounty, selectedTab }: BountyTabsProps) {
   return (
     <div>
       <div className="w-full sm:block">
@@ -27,35 +123,9 @@ export default function BountyTabs({ bounty, selectedTab }: Props) {
             className="no-scrollbar -mb-px flex space-x-8 overflow-x-auto"
             aria-label="Tabs"
           >
-            {tabs.map((tab) => (
-              <Link
-                replace={true}
-                key={tab.name}
-                href={`?tab=${tab.name.toLowerCase()}`}
-                className={cn(
-                  selectedTab === tab.name.toLowerCase()
-                    ? "border-indigo-600 text-indigo-600 dark:border-indigo-500 dark:text-indigo-500"
-                    : "border-transparent text-muted-foreground hover:border-primary-foreground hover:text-primary-foreground",
-                  "group inline-flex items-center border-b px-1 py-4 text-sm font-medium",
-                )}
-              >
-                <tab.icon
-                  className={cn(
-                    selectedTab === tab.name.toLowerCase()
-                      ? "text-indigo-600 dark:text-indigo-500"
-                      : "text-muted-foreground group-hover:text-primary-foreground",
-                    "-ml-0.5 mr-2 h-5 w-5",
-                  )}
-                />
-
-                <span className="flex gap-x-1">
-                  {tab.name === "applications" && bounty && (
-                    <ApplicationCount bounty={bounty} />
-                  )}
-                  <span>{capitalizeFirstLetter(tab.name)}</span>
-                </span>
-              </Link>
-            ))}
+            <DetailTab selectedTab={selectedTab} bounty={bounty} />
+            <ApplicationTab selectedTab={selectedTab} bounty={bounty} />
+            <DiscussionTab selectedTab={selectedTab} bounty={bounty} />
           </nav>
         </div>
       </div>
